59a0d0424cf5571d84bc7f9d964fee816bb5cebf
afonsojrm <afonsof00@gmail.com>
2022-08-18 08:56:36 -0300

    Fatorial e arquivo .gitignore

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+int fatorial(int n){
+    if(n == 0){
+        return 1;
+    }else {
+        return n*fatorial(n-1);
+    }
+}
 int main(){
-    printf("Hello Git\n");
+    printf("Hello git\n");
+    printf("Fatorial(5) = %d\n", fatorial (5));
 }
